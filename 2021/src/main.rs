6337f8d877129918aab73a4b8df6da2f48b91706
Micha K <micha@kuehn.io>
2022-10-06 01:25:10 +0200

    add windowed function (w/ tests)

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,21 +1,29 @@
 use std::fs::read_to_string;
 
-use submarine::sonar::SonarReadings;
+use submarine::sonar::{self, SonarReadings};
 
 pub mod submarine;
 
 fn main() {
-    day1_puzzle1();
+    let sonar_readings = SonarReadings(
+        read_to_string("input/day1_sonar_sweep")
+            .unwrap()
+            .lines()
+            .map(|r| r.parse::<i32>().unwrap())
+            .collect(),
+    );
+
+    day1_puzzle1(sonar_readings.clone());
+    day1_puzzle2(sonar_readings.clone());
 }
 
-fn day1_puzzle1() {
-    let sonar_readings = read_to_string("input/day1_sonar_sweep")
-        .unwrap()
-        .lines()
-        .map(|r| r.parse::<i32>().unwrap())
-        .collect();
+fn day1_puzzle1(sonar_readings: SonarReadings) {
+    println!("depth increases: {}", sonar_readings.depth_increases());
+}
+
+fn day1_puzzle2(sonar_readings: SonarReadings) {
     println!(
-        "depth increases: {}",
-        SonarReadings(sonar_readings).depth_increases()
+        "depth increases (3-measurement window): {}",
+        sonar_readings.depth_increases_windowed(3)
     );
 }
--- 2021/src/submarine/sonar.rs
+++ 2021/src/submarine/sonar.rs
@@ -2,6 +2,11 @@ type SonarReading = i32;
 
 pub struct SonarReadings(pub Vec<SonarReading>);
 
+impl Clone for SonarReadings {
+    fn clone(&self) -> Self {
+        Self(self.0.to_owned())
+    }
+}
 impl SonarReadings {
     pub fn depth_increases(&self) -> i32 {
         self.0.iter().zip(self.0.iter().skip(1)).fold(
@@ -14,6 +19,19 @@ impl SonarReadings {
                 }
             },
         )
+    }
+
+    pub fn depth_increases_windowed(&self, window_size: usize) -> i32 {
+        self.0
+            .windows(window_size)
+            .zip(self.0.windows(window_size).skip(1))
+            .fold(0, |sum, (current, next)| {
+                if next.iter().sum::<i32>() > current.iter().sum::<i32>() {
+                    sum + 1
+                } else {
+                    sum
+                }
+            })
     }
 }
 
@@ -43,5 +61,36 @@ mod tests {
     fn only_decreasing() {
         let readings = SonarReadings(vec![10, 9, 8, 7, 6]);
         assert_eq!(readings.depth_increases(), 0);
+    }
+
+    #[test]
+    fn multiple_readings_continously_increasing_windows() {
+        let readings = SonarReadings(vec![1, 2, 3, 4]);
+        assert_eq!(readings.depth_increases_windowed(3), 1);
+    }
+
+    #[test]
+    fn only_decreasing_windowed() {
+        let readings = SonarReadings(vec![10, 9, 8, 7, 6]);
+        assert_eq!(readings.depth_increases_windowed(3), 0);
+    }
+
+    #[test]
+    fn multiple_readings_mixed_order_windowed() {
+        /*
+        0 5 10 = 15 (no previous sum)
+        5 10 5 = 20 (increased)
+        10 5 10 = 25 (increased)
+        5 10 11 = 26 (increased)
+        10 11 10 = 31 (increased)
+         */
+        let readings = SonarReadings(vec![0, 5, 10, 5, 10, 11, 10]);
+        assert_eq!(readings.depth_increases_windowed(3), 4);
+    }
+
+    #[test]
+    fn no_readings_windowed() {
+        let readings = SonarReadings(vec![]);
+        assert_eq!(readings.depth_increases_windowed(3), 0);
     }
 }
