d5d330908888dfcffcbd9c8fae557932e2ee08fe
Micha K <micha@kuehn.io>
2022-10-06 02:06:38 +0200

    added test for puzzle results

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -27,3 +27,36 @@ fn day1_puzzle2(sonar_readings: SonarReadings) {
         sonar_readings.depth_increases_windowed(3)
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::read_to_string;
+
+    use crate::submarine::sonar::SonarReadings;
+
+    #[test]
+    fn day1_puzzle1_solution() {
+        let sonar_readings = SonarReadings(
+            read_to_string("input/day1_sonar_sweep")
+                .unwrap()
+                .lines()
+                .map(|r| r.parse::<usize>().unwrap())
+                .collect(),
+        );
+
+        assert_eq!(sonar_readings.depth_increases(), 1215);
+    }
+
+    #[test]
+    fn day1_puzzle2_solution() {
+        let sonar_readings = SonarReadings(
+            read_to_string("input/day1_sonar_sweep")
+                .unwrap()
+                .lines()
+                .map(|r| r.parse::<usize>().unwrap())
+                .collect(),
+        );
+
+        assert_eq!(sonar_readings.depth_increases_windowed(3), 1150);
+    }
+}
